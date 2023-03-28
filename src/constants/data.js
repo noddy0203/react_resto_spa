271166c68c5d63038e233afc59d8be458752c909
Amar Singh Kushwaha <sourabh.ask12@gmail.com>
2023-03-28 20:21:20 +0530

    adding assets and main css

---
+++ src/constants/data.js
@@ -0,0 +1,85 @@
+import images from "./images"
+
+const wines = [
+    {
+        title: "Chapel Hill Shiraz",
+        price: "$56",
+        tags: "AU | Bottle"
+    },
+    {
+        title: "Catena Malbee",
+        price: "$59",
+        tags: "AU | Bottle"
+    }
+    ,
+    {
+        title: "La vieillw Rose",
+        price: "$44",
+        tags: "FR | 750ml"
+    },
+    {
+        title: "Rhino Pale Ale",
+        price: "$44",
+        tags: "CA | 750ml"
+    },
+    {
+        title: "Irish Guinness",
+        price: "$26",
+        tags: "IE | 750ml"
+    }
+];
+
+const cocktails = [
+    {
+        title: "Aperol Aprtiz",
+        price: "$20",
+        tags: "Aperol | Villa Marchesi prosecco | soda | 30 ml"
+    },
+    {
+        title: "Dark 'N' Stormy",
+        price: '$16',
+        tags: 'Dark rum | Ginger beer | Slice of lime',
+    },
+    {
+        title: 'Daiquiri',
+        price: '$10',
+        tags: 'Rum | Citrus juice | Sugar',
+    },
+    {
+        title: 'Old Fashioned',
+        price: '$31',
+        tags: 'Bourbon | Brown sugar | Angostura Bitters',
+    },
+    {
+        title: 'Negroni',
+        price: '$26',
+        tags: 'Gin | Sweet Vermouth | Campari | Orange garnish',
+    },
+];
+
+const awards = [
+    {
+        imgUrl: images.award02,
+        title: 'Bib Gourmond',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    },
+    {
+        imgUrl: images.award01,
+        title: 'Rising Star',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    },
+    {
+        imgUrl: images.award05,
+        title: 'AA Hospitality',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    },
+    {
+        imgUrl: images.award03,
+        title: 'Outstanding Chef',
+        subtitle: 'Lorem ipsum dolor sit amet, consectetur.',
+    },
+]
+
+export default {
+    wines, cocktails, awards
+}
---
+++ src/constants/images.js
@@ -0,0 +1,47 @@
+import award01 from "../assets/award01.png"
+import award02 from "../assets/award02.png"
+import award03 from "../assets/award03.png"
+import award05 from "../assets/award05.png"
+import bg from "../assets/bg.png"
+import chef from "../assets/chef.png"
+import findus from "../assets/findus.png"
+import G from "../assets/G.png"
+import gal1 from "../assets/gallery01.png"
+import gal2 from "../assets/gallery02.png"
+import gal3 from "../assets/gallery03.png"
+import gal4 from "../assets/gallery04.png"
+import gericht from "../assets/gericht.png"
+import knife from "../assets/knife.png"
+import laurels from "../assets/laurels.png"
+import logo from "../assets/logo.png"
+import menu from "../assets/menu.png"
+import overlaybg from "../assets/overlaybg.png"
+import quote from "../assets/quote.png"
+import sign from "../assets/sign.png"
+import spoon from "../assets/spoon.png"
+import welcome from "../assets/welcome.png"
+
+export default {
+    award01,
+    award02,
+    award03,
+    award05,
+    chef,
+    bg,
+    findus,
+    G,
+    gal1,
+    gal2,
+    gal3,
+    gal4,
+    gericht,
+    knife,
+    laurels,
+    logo,
+    menu,
+    overlaybg,
+    quote,
+    sign,
+    spoon,
+    welcome
+}
