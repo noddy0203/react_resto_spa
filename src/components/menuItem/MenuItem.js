93d3a9e93a2c329cb1d10d635f3428ad98dd8dbc
Amar Singh Kushwaha <sourabh.ask12@gmail.com>
2023-04-25 13:26:20 +0530

    menu css updated

--- src/components/menuItem/MenuItem.js
+++ src/components/menuItem/MenuItem.js
@@ -1,9 +1,27 @@
 import React from 'react'
 import "./menuItem.css"
 
-const MenuItem = () => {
+const MenuItem = ({ title, tags, price }) => {
   return (
-    <div>MenuItem</div>
+    <>
+      <div className="app_menuitem">
+        <div className="app_menuitem-head">
+          <div className="app_menuitem-name">
+            <p className="p_cormorant" style={{ color: "#DCCA87" }}>{title}</p>
+          </div>
+          
+          <div className='app_menuItem-dash' />
+
+          <div className="app_menuitem-price">
+            <p className="p-cormorant" style={{ color: "#DCCA87" }}>{price}</p>
+          </div>
+        </div>
+        <div className="app_menuitem-sub">
+          <p className="p_opensans" style={{ color: "#AAA" }}>{tags}</p>
+        </div>
+
+      </div>
+    </>
   )
 }
 
